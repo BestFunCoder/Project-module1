b3098b795d84b4452ee7c946751a751846705c15
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-17 07:40:01 +0300

    Add CaesarCipher.java

---
+++ src/cipher/CaesarCipher.java
@@ -0,0 +1,15 @@
+package src.cipher;
+
+public class CaesarCipher {
+
+    public String shiftText(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+
+            }
+
+        }
+        return result.toString();
+    }
+}
