ab980c2c939f06f045a511869bfc9457fe8d50b0
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-17 07:38:23 +0300

    Add Application.java

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+package src;
+
+import src.runner.ApplicationRunner;
+
+public class Application {
+    public static void main(String[] args) {
+        new ApplicationRunner().run(args);
+    }
+}
