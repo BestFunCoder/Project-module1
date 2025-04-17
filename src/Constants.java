b9e4a36620152262b4082bfff5c39fdb6ad5047e
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-17 07:43:47 +0300

    Add packages with exceptions classes

---
+++ src/Constants.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.util.List;
+
+public class Constants {
+
+    public static List<String> ALLOWED_KEY = List.of("ENCRYPT", "DECRYPT","BRUTE_FORCE");
+
+    public static final int COMMAND_TYPE_ARG_POSITION = 0;
+    public static final int FILE_ARG_POSITION = 1;
+    public static final int KEY_ARG_POSITION = 1;
+    public static final int MAX_ALLOWED_ARGS_COUNT = 2;
+}
