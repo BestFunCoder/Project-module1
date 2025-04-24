2d8c2de218c1081c0f68e220ff3857674dd31070
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-24 18:07:37 +0300

    Update ApplicationRunner Class -run(...) — manages the encryption/decryption process. -isFileExist() — checks the file path validity. -getFileContent() — reads the file content. -saveResultToFile() — saves the result to a new file.

--- src/runner/ApplicationRunner.java
+++ src/runner/ApplicationRunner.java
@@ -3,51 +3,48 @@ package src.runner;
 import src.CommandType;
 import src.cipher.BruteForceAction;
 import src.cipher.CaesarCipherAction;
-import src.cipher.DecryptCaesar;
+import src.cipher.DecryptionAction;
 import src.cipher.EncryptionAction;
 import src.exceptions.InvalidFileContentException;
 import src.exceptions.WrongFilePathException;
+import src.util.FileNameUtil;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static src.CommandType.*;
 import static src.Constants.*;
-import static src.util.CipherKeyValidatorUtil.isArgumentsValid;
+import static src.util.CipherCommandValidatorUtil.isArgumentsValid;
 
 public class ApplicationRunner {
+    int keyCipher;
 
     public void run(String[] args) {
-
-        // java -jar MyApp.jar [ENCRYPT|DECRYPT] <path_to_file> [KEY]
-        //java -jar MyApp.jar ENCRYPT <path_to_file> [KEY]
-        //java -jar MyApp.jar <path_to_file> [KEY]
-
-        if (args.length < MAX_ALLOWED_ARGS_COUNT) {
-            //throw exception
+        if (args.length < MIN_ALLOWED_ARGS_COUNT || args.length > MAX_ALLOWED_ARGS_COUNT) {
+            throw new IllegalArgumentException("Not enough arguments." + "Correct format: ENCRYPT|DECRYPT  [KEY]" + "BRUTE_FORCE <path_to_file>" + "The program is ending.");
         }
 
         CommandType commandType = isArgumentsValid(args[COMMAND_TYPE_ARG_POSITION]);
-
         String filePath = args[FILE_ARG_POSITION];
         Path path = Path.of(filePath);
         isFileExist(path);
         String content = getFileContent(path);
-        if(commandType != BRUTE_FORCE) {
 
+        if (commandType == DECRYPT && !filePath.contains("[ENCRYPTED]")) {
+            throw new IllegalArgumentException("For decryption, the file must contain [ENCRYPTED] in its name.");
         }
 
-        int key = Integer.parseInt(args[KEY_ARG_POSITION]);
+        if (commandType != BRUTE_FORCE) {
+            keyCipher = Integer.parseInt(args[KEY_ARG_POSITION]);
+        }
 
-        Map<CommandType, CaesarCipherAction> operations = Map.of(
-                ENCRYPT, new EncryptionAction(key),
-                DECRYPT, new DecryptCaesar(key),
-                BRUTE_FORCE, new BruteForceAction());
-
-        operations.get(commandType).execute(content);
+        Map<CommandType, CaesarCipherAction> operations = Map.of(ENCRYPT, new EncryptionAction(keyCipher), DECRYPT, new DecryptionAction(keyCipher), BRUTE_FORCE, new BruteForceAction());
+        String result = operations.get(commandType).execute(content);
+        saveResultToFile(result, path, commandType);
     }
 
     private void isFileExist(Path path) {
@@ -57,10 +54,29 @@ public class ApplicationRunner {
     }
 
     private String getFileContent(Path path) {
-        try {
-            return Files.lines(path).collect(Collectors.joining("\n"));
+        try (Stream<String> lines = Files.lines(path)) {
+            return lines.collect(Collectors.joining("\n"));
         } catch (IOException e) {
-            throw new InvalidFileContentException("Wrong file content");
+            throw new InvalidFileContentException("Wrong file content.");
+        }
+    }
+
+    private void saveResultToFile(String resultText, Path inputPath, CommandType command) {
+        try {
+            if (command == BRUTE_FORCE && resultText.trim().isEmpty()) {
+                System.out.println("The file could not be decrypted using brute force.");
+                return;
+            }
+
+            Path parentDir = inputPath.getParent();
+            String newFileName = FileNameUtil.getNewFileName(inputPath, command);
+            Path resultPath = parentDir.resolve(newFileName);
+
+            Files.writeString(resultPath, resultText);
+            System.out.println("The result is saved to a file: " + resultPath);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to save result: " + e.getMessage(), e);
         }
     }
 }
