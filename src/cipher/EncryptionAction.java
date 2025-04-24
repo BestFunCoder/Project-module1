a767e1f6f09fa99639da304bbe1031033efe552b
Oleh Hutsol <oleggutsol80@gmail.com>
2025-04-24 18:34:26 +0300

    Implemented EncryptionAction.java to handle encryption logic

--- src/cipher/EncryptionAction.java
+++ src/cipher/EncryptionAction.java
@@ -1,15 +1,15 @@
 package src.cipher;
 
 public class EncryptionAction extends CaesarCipherAction {
-    private final int key;
+    private final int keyCipher;
     private final CaesarCipher cipher = new CaesarCipher();
 
-    public EncryptionAction(int key) {
-        this.key = key;
+    public EncryptionAction(int keyCipher) {
+        this.keyCipher = keyCipher;
     }
 
     @Override
     public String execute(String text) {
-        return cipher.shiftText(text, key);
+        return cipher.shiftText(text, keyCipher);
     }
 }
